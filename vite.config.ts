9a3310c1e8e059ee86739a06d8a1fd55f036d8ed
Lee, Dénes <leedenes12@gmail.com>
2026-09-22 21:57:19 +0200

    feat: deploy to gh pages
    Signed-off-by: Lee, Dénes <leedenes12@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "mantine-dashboard-builder",
   plugins: [
     tanstackRouter({ target: 'react', autoCodeSplitting: true, routesDirectory: 'src/routes' }),
     react(),
